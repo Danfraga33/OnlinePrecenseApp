983ae7d25ec15a7dcf2632c61aa6ab3e72c10046
Daniel Fraga <dan.fraga33@gmail.com>
2023-11-03 10:34:19 +0000

    feat: AppLayout - sidebar for dashboard

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	const getLayout = Component.getLayout || ((page) => page);
+	return (
+		<div>
+			<getLayout>
+				{getLayout(<Component {...pageProps} />, pageProps)}
+			</getLayout>
+		</div>
+	);
 }
